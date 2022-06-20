53895b443438ec17edf1d65bbab74d5e05f205e6
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-06-20 16:04:40 -0500

    well the search only works once on the character page but that's something i spose

--- src/components/character/Character.js
+++ src/components/character/Character.js
@@ -1,5 +1,6 @@
 import { CharacterForm } from "./CharacterForm"
 import { ManageCharacters } from "./ManageCharacters"
+import {SearchCharacters} from "./SearchCharacters"
 import { getAllFactions, getAllCharacters, getAllRoles, getAllWeapons, getAllServers, saveNewCharacter, getUserCharacters } from "../APIManager"
 import { useState, useEffect } from "react"
 // import { useEffect, useState } from "react"
@@ -17,6 +18,7 @@ export const Character = () => {
     const [roles, setRoles] = useState([])
     const [feedback, setFeedback] = useState("")
     const [userCharacters, updateUserCharacters] = useState([])
+    const [searchWords, setSearch] = useState("")
    
     useEffect(
         () => {
@@ -42,7 +44,15 @@ export const Character = () => {
         },
         [] // When this array is empty, you are observing initial component state
     )
-
+    useEffect(
+        () => {
+            const searchedChar = userCharacters.filter(character => {
+                return character.character.toLowerCase().startsWith(searchWords.toLowerCase())  //make both lowercase so you can always find a match regardless of case
+            })
+            updateUserCharacters(searchedChar)
+        },
+        [searchWords]//find what you put into the search bar and set that as sorted
+    ) 
 
 
    useEffect(() => {
@@ -54,6 +64,7 @@ export const Character = () => {
  return <>
     <CharacterForm factions={factions} setFactions={setFactions} RosterUserObject={RosterUserObject} updateUserCharacters={updateUserCharacters}weapons={weapons} setWeapons={setWeapons} servers={servers} roles={roles} feedback={feedback} setFeedback={setFeedback}/>
      <h2 className="characterForm__title">Edit Characters</h2>
+     <div><SearchCharacters setSearch={setSearch}/></div> 
   <section className="edit_characters">
  
    <ManageCharacters feedback={feedback} userCharacters={userCharacters} updateUserCharacters={updateUserCharacters} setFeedback={setFeedback} 
