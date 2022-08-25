3460d7d28c4432cfa2c59cf49e8229cbc361dd3d
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-08-25 10:26:38 -0500

    organized the user update form so it looks a lil better

--- src/components/profile/ProfileForm.js
+++ src/components/profile/ProfileForm.js
@@ -19,9 +19,9 @@ export const UpdateUser = () => {
     })
     useEffect(
         () => {
-          getUserbyId(localUser.id)
+        getUserbyId(localUser.id)
             .then((data) => {
-              setUser(data)
+            setUser(data)
             })
         },
         []
@@ -58,30 +58,29 @@ export const UpdateUser = () => {
                 <h1 className="h3 mb-3 font-weight-normal">Update Profile</h1>
 
 
-                <fieldset>
+                <fieldset className="form--update">
                     <label htmlFor="email"></label>
                     <input onChange={(evt) => userUpdate(evt)}
-                        type="email" name="email" className="form-control"
+                        type="email" name="email" className="form-control--update"
                         value={user.email}
                         placeholder="Email address" required />
                     <label htmlFor="username"></label>
                         <input onChange={(evt) => userUpdate(evt)}
-                        type="text" name="username" className="form-control"
+                        type="text" name="username" className="form-control--update"
                         value={user.username}
                         placeholder="" required />
                     <label htmlFor="First Name"></label>
                     <input onChange={(evt) => userUpdate(evt)}
-                    type="text" name="first_name" className="form-control"
+                    type="text" name="first_name" className="form-control--update"
                     value={user.first_name} placeholder="" required />
                 <label htmlFor="lastName"></label>
                 <input onChange={(evt) => userUpdate(evt)}
-                type="text" name="last_name" className="form-control"
+                type="text" name="last_name" className="form-control--update"
                 value={user.last_name} placeholder="" required />
                 </fieldset>
 
-                <fieldset>
                     <button className="email__button" type="submit" onClick={handleSaveButtonClick}> Update </button>
-                </fieldset>
+                
             </form>
             <div>  <h4>Saved Rosters</h4>
                 <SavedRosters localUser={localUser} /></div>
