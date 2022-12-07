2249a18e2591a0cd74ff5de7eb6441b8ae05fedf
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-12-07 07:17:36 -0600

    sort by group number

--- src/components/ViewStats/StatFilters.js
+++ src/components/ViewStats/StatFilters.js
@@ -3,20 +3,27 @@ import { RoleSelect } from "../character/Role"
 import { getAllRoles } from "../APIManager"
 import { useEffect, useState } from "react"
 
-export const StatFilters = ({players}) => {
+export const StatFilters = ({players, setFilteredPlayers}) => {
     const [roles, setRoles] = useState([])
 useEffect(() => {
     getAllRoles(setRoles)
 }, []) 
 
-function sortGroup(){
-    
+function compareNumbers(a, b) {
+    return a.group - b.group;
+  }
+function sortGroup(click){
+    click.preventDefault()
+    const copy = [...players]
+copy.sort(compareNumbers)
+setFilteredPlayers(copy)
 }
+
 
 
     return <>
        <div>
-            <button className='button-84' onClick={sortGroup()}>Group</button>
+            <button className='button-84' onClick={(click) => {sortGroup(click)}}>Group</button>
             <button className='button-84'>Damage</button>
             <button className='button-84'>Healing</button>
             <button className='button-84'>Kills</button>
