28bf63a8f0d05be9371e7ca43d0c09000af30c23
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-03-23 13:39:09 -0500

    displaying in the correct groups on render

--- src/components/roster/RosterGrid.js
+++ src/components/roster/RosterGrid.js
@@ -1,69 +1,75 @@
 import "./rostergrid.css"
 import React from "react";
-import { getRosterCharacter, getCurrentRoster, newRosterChoice  } from "../managers/RosterManager";
+import { getRosterCharacter, getCurrentRoster, newRosterChoice } from "../managers/RosterManager";
 import { useEffect, useState } from "react";
 import { RosterDiv } from "./RosterDiv";
-import { getAllCharacters} from "../managers/CharacterManager";
+import { getAllCharacters } from "../managers/CharacterManager";
 import { RosterDivForEdit } from "./RosterDivForEdit";
 import "./rostergrid.css"
 
 
 //get all the people in the roster render them in the list
 
-export const RosterGrid = ({showText, nestedEditRosterCharacters, setShowText, charId, setNewRosterPick, setCharId, handleMouseEnter, handleMouseLeave, setEditCharacters, editRosterCharacters, 
+export const RosterGrid = ({ showText, nestedEditRosterCharacters, setShowText, charId, setNewRosterPick, setCharId, handleMouseEnter, handleMouseLeave, setEditCharacters, editRosterCharacters,
     newRosterPicks, rosterIDNUMBER, characters }) => {
-        
+
     useEffect(
         () => {
             if (rosterIDNUMBER) {
-            getCurrentRoster(rosterIDNUMBER)
-                .then((res) => {
-                    setEditCharacters(res)
-                })}}
+                getCurrentRoster(rosterIDNUMBER)
+                    .then((res) => {
+                        setEditCharacters(res)
+                    })
+            }
+        }
         ,
         []
     )
 
     const handleDragOver = (e) => {
         e.preventDefault();
-      };
+    };
     const handleDrop = (e, groupIndex) => {
         e.preventDefault();
         console.log(groupIndex)
         const characterData = e.dataTransfer.getData('character');
         const character = JSON.parse(characterData);
-      
-        if (!editRosterCharacters.find((player) => player.id === character.id)) {
-          if (editRosterCharacters.length < 50) {
-            const new_choice = { roster: rosterIDNUMBER, character: character.id, group: groupIndex + 1}
-            // I also need to add a group number to the new choice, I want to be able to use the group number displayed on screen so the user can see where the character is going
 
-        newRosterChoice(new_choice) 
-            .then(() => { getCurrentRoster(rosterIDNUMBER).then((res) => { setEditCharacters(res) }) })
-          } else {
-            alert('Roster is full');
-          }
+        if (!editRosterCharacters.find((player) => player.id === character.id)) {
+            if (editRosterCharacters.length < 50) {
+                const new_choice = { roster: rosterIDNUMBER, character: character.id, group: groupIndex + 1 }
+                // I also need to add a group number to the new choice, I want to be able to use the group number displayed on screen so the user can see where the character is going
+
+                newRosterChoice(new_choice)
+                    .then(() => { getCurrentRoster(rosterIDNUMBER).then((res) => { setEditCharacters(res) }) })
+            } else {
+                alert('Roster is full');
+            }
         }
-      };
-      const totalGroups = 10;
-const emptyGroups = new Array(totalGroups).fill([]);
-const allGroups = emptyGroups.map((emptyGroup, index) => {
-    return nestedEditRosterCharacters[index] || emptyGroup;
-  });
-  
+    };
+    // const totalGroups = 10;
+    // const emptyGroups = new Array(totalGroups).fill([]);
+    // const allGroups = emptyGroups.map((emptyGroup, index) => {
+    //     return nestedEditRosterCharacters[index] || emptyGroup;
+    // });
+    const totalGroups = 10;
+
+    const allGroups = Array.from({ length: totalGroups }, (_, groupIndex) => {
+      return editRosterCharacters.filter((c) => c.group === groupIndex + 1);
+    });
 
 
     return (
         <>
             {allGroups.map((group, groupIndex) => (
-                <div  onDragOver={handleDragOver}
-                onDrop={(e) => handleDrop(e, groupIndex)}
-                key={groupIndex} className="grid__item">
+                <div onDragOver={handleDragOver}
+                    onDrop={(e) => handleDrop(e, groupIndex)}
+                    key={groupIndex} className="grid__item">
                     <span className="roster__group">group {groupIndex + 1}</span>
                     {group.map((c) => (
                         <RosterDivForEdit
-                        getCurrentRoster={getCurrentRoster} showText={showText} setShowText={setShowText} charId={charId} setCharId={setCharId} handleMouseEnter={handleMouseEnter} 
-                         handleMouseLeave={handleMouseLeave} rosterIDNUMBER={rosterIDNUMBER} setEditCharacters={setEditCharacters} characters={characters} newRosterPicks={editRosterCharacters}
+                            getCurrentRoster={getCurrentRoster} showText={showText} setShowText={setShowText} charId={charId} setCharId={setCharId} handleMouseEnter={handleMouseEnter}
+                            handleMouseLeave={handleMouseLeave} rosterIDNUMBER={rosterIDNUMBER} setEditCharacters={setEditCharacters} characters={characters} newRosterPicks={editRosterCharacters}
                             setNewRosterPick={setNewRosterPick}
                             key={c.id}
                             c={c}
@@ -76,7 +82,7 @@ const allGroups = emptyGroups.map((emptyGroup, index) => {
 
 
 
-}   
+}
 
 
 
