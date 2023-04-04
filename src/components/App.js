279f658db2861bfe280730197f5d0005bcea44d1
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-04-04 13:08:08 -0500

    coming along pretty quick ngl

--- src/components/App.js
+++ src/components/App.js
@@ -10,28 +10,94 @@ import { PublicBoardsContainer } from './public/PublicBoardsContainer';
 import { PublicViewStats } from './public/PublicViewStats';
 import { Faq } from './faq/Faq';
 import { AddSharedCharacter } from './public/AddSharedCharacter';
+import { PublicNavBar } from './Nav/PublicNavBar';
 
-export const App = () => {
-	return <Routes>
-		<Route path="/login" element={<Login />} />
-		<Route path="/register" element={<Register />} />
-		<Route path="" element={<Main />}/>
-		<Route path="/public" element={<PublicBoardsContainer />} />
-		<Route path="/public/:calculatedRosterId" element={<PublicViewStats />} />
-		<Route path="/faq" element={<Faq />} />
-		<Route path="/add_character/:tokenId" element={<AddSharedCharacter />} />
+// export const App = () => {
+// 	return <Routes>
+// 		<Route path="/login" element={<Login />} />
+// 		<Route path="/register" element={<Register />} />
+// 		<Route path="" element={<Main />}/>
+// 		<Route path="/public" element={<PublicBoardsContainer />} />
+// 		<Route path="/public/:calculatedRosterId" element={<PublicViewStats />} />
+// 		<Route path="/faq" element={<Faq />} />
+// 		<Route path="/add_character/:tokenId" element={<AddSharedCharacter />} />
 		
 
-		<Route path="*" element={
+// 		<Route path="*" element={
+// 			<Authorized>
+// 				<>
+// 					<NavBar />
+// 					<ApplicationViews />
+// 				</>
+// 			</Authorized>
+
+// 		} />
+// 	</Routes>
+// }
+export const App = () => {
+	return (
+	  <Routes>
+		<Route path="/login" element={<Login />} />
+		<Route path="/register" element={<Register />} />
+		<Route
+		  path=""
+		  element={
+			<>
+			  <PublicNavBar />
+			  <Main />
+			</>
+		  }
+		/>
+		<Route
+		  path="/public"
+		  element={
+			<>
+			  <PublicNavBar />
+			  <PublicBoardsContainer />
+			</>
+		  }
+		/>
+		<Route
+		  path="/public/:calculatedRosterId"
+		  element={
+			<>
+			  <PublicNavBar />
+			  <PublicViewStats />
+			</>
+		  }
+		/>
+		<Route
+		  path="/faq"
+		  element={
+			<>
+			  <PublicNavBar />
+			  <Faq />
+			</>
+		  }
+		/>
+		<Route
+		  path="/add_character/:tokenId"
+		  element={
+			<>
+			  <PublicNavBar />
+			  <AddSharedCharacter />
+			</>
+		  }
+		/>
+  
+		<Route
+		  path="*"
+		  element={
 			<Authorized>
-				<>
-					<NavBar />
-					<ApplicationViews />
-				</>
+			  <>
+				<NavBar />
+				<ApplicationViews />
+			  </>
 			</Authorized>
-
-		} />
-	</Routes>
-}
-
+		  }
+		/>
+	  </Routes>
+	);
+  };
+  
 
--- src/components/public/PublicBoardsContainer.js
+++ src/components/public/PublicBoardsContainer.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { getPublicCalculatedRosters } from "../managers/PublicManager"
 import { Link } from "react-router-dom"
+import "./public.css"
 export const PublicBoardsContainer = () => {
 const [warStats, setWarStats] = useState([])
 
@@ -11,20 +12,46 @@ const [warStats, setWarStats] = useState([])
         []
     )
 
-    return <div className="parent">
-        <div className="war--stats--list--container">
+    return (
+        <div className="parent--public--boards">
+            <div className="left--container--public">
+          
             PUBLIC BOARDS
-        {warStats.map((stat) => {
-            return <div key={stat.id}>
-                <div className="public--rosters"><Link to={`/public/${stat.id}`}>{
-                    stat.rosterName ? `${stat.rosterName}` : "Untitled War Stats"
-                }</Link>
-            </div></div>
-        })}</div>
+            {warStats.map((stat) => {
+              return (
+                <div className="list--parent" key={stat.id}>
+                  <div className="public-rosters">
+                    <Link to={`/public/${stat.id}`}>
+                      {stat.rosterName ? `${stat.rosterName}` : "Untitled War Stats"}
+                    </Link>
+                  </div>
+                </div>
+              );
+            })}</div>
+         
+      
+          <div className="right-container--public">
+            <div className="twitch-parent">
+            <div className="video-parent">
+  <iframe
+    className="video-iframe"
+    src="https://player.twitch.tv/?channel=yaboiwiilly&parent=localhost"
+    scrolling="no"
+    allowFullScreen="true"
+  ></iframe>
+</div>
+<div className="chat-parent">
+  <iframe
+    className="chat-iframe"
+    scrolling="no"
+    id="chat_embed"
+    src="https://www.twitch.tv/embed/yaboiwiilly/chat?parent=localhost"
+  ></iframe>
+</div>
 
-        <div className="right--container">
-
-            we could put a little banner for willy on the side :D
-        </div>
-    </div>
+            </div>
+          </div>
+        </div> 
+      );
+      
 }
