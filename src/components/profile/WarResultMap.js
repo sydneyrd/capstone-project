5b1ba7f749731dd6d97b229bc9c7f417f13ca638
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-04-03 21:51:10 -0500

    button that directs to edit form in list

--- src/components/profile/WarResultMap.js
+++ src/components/profile/WarResultMap.js
@@ -1,19 +1,20 @@
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
 import { deleteCalculatedRoster } from "../managers/CalculatedRosterManager"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTrashCan} from '@fortawesome/free-solid-svg-icons'
 import { faShareFromSquare } from "@fortawesome/free-solid-svg-icons"
 import { faRectangleXmark } from "@fortawesome/free-solid-svg-icons"
-
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate } from "react-router-dom"
 import { useContext } from "react"
 import { calculateContext } from "../views/ApplicationViews"
 export const WarResultMap = ({ stat, getUserWarStats, setUserWarStats, publishRoster }) => {
       // Use the useContext hook to access the current value and update function of the second context
 const { setCurrentCalculateRoster } = useContext(calculateContext);
 
-  library.add(faTrashCan, faShareFromSquare, faRectangleXmark)
- 
+  library.add(faTrashCan, faShareFromSquare, faRectangleXmark, faPenToSquare)
+ let navigate = useNavigate()
 
   const handleDeleteClick = (click, stat) => {
     click.preventDefault()
@@ -36,6 +37,11 @@ const { setCurrentCalculateRoster } = useContext(calculateContext);
     }
   };
 
+const handleEditClick = (click) => {
+  click.preventDefault()
+  setCurrentCalculateRoster(stat.id)
+  navigate(`/resources/edit/${stat.id}`)
+}
 
   return <div className="saved--rosters"><Link onClick={click => setCurrentCalculateRoster(stat.id)}to={`/resources/${stat.id}/view`}><div className="roster__link" to="/roster">{stat.rosterName ? `${stat.rosterName}` : "War Stats"}</div></Link>
     <FontAwesomeIcon className="delete__roster" onClick={click => handleDeleteClick(click, stat)} icon="fa-solid fa-trash-can" />
@@ -44,9 +50,14 @@ const { setCurrentCalculateRoster } = useContext(calculateContext);
   stat.is_public ? <FontAwesomeIcon className="publish__roster" onClick={click => handlePublish(click, false, stat.id)} icon="fa-solid fa-rectangle-xmark" /> : <FontAwesomeIcon className="publish__roster" onClick={click => handlePublish(click, true, stat.id)} icon="fa-solid fa-share-from-square" />
 }
 
-    {/* icon to link to edit view
 
-    icon to make public */}
+    
+
+    <FontAwesomeIcon className="edit__roster"
+    
+    //navigate to edit page
+    onClick={handleEditClick}
+    icon="fa-solid fa-pen-to-square" /> 
 
 
 
