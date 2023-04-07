eed7de9162caff521fd64dcb3989eb329d2230e8
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-04-07 14:59:42 -0500

    updated

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -6,19 +6,20 @@ export const Faq = () => {
         <div className="faq--container">
             <h1>FAQ</h1>
             <h2>What is this?</h2>
-            <p>This is a tool for tracking your roster's performance in war. It is a work in progress and a solo project, so please be patient with me as I add features and fix bugs.</p>
+            <p>This is a tool for tracking your roster's performance in war. It is a work in progress and a solo project that I am just building for fun, so please be patient with me as I add features and fix bugs.</p>
             
             <h2>How do I use it?</h2>
             <p>
                 You can view war boards that have been made public by clicking the "view public boards" button on the main page.
-                you'll need to create an account to create a roster, view characters, or enter war stats.  
+                you'll need to create an account to create a roster, view characters, or enter war stats. 
+                Once you've created an account, you can view all created characters. 
             </p>
             <h2>I found a bug</h2>
             <p>Great!  Please contact me and let me know so I can fix it.</p>
             <h2>When will *whatever* feature be added?</h2>
-            <p>When I have time to work on it.  I'm a one person team, so I'm working on this in my spare time. I have quite a few plans, but please contact me and let me know what you think should take priority</p>
+            <p>Hopefully soon, but depends on how much time I have to work on this. I am working on specific features, but would love to hear what you would like to see prioritized.</p>
 <h2>I have some other question</h2>
-<p>Great!  Please contact me.</p>
+<p>Great!  Please contact me, I don't know what to put here.</p>
 
             </div>
             <div className="contact--container">
